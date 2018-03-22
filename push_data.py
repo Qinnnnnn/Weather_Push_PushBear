144bb25c75fb6c89aa089aa6294dec4a4c9347f3
Clever_Moon <qzr19970105@outlook.com>
2018-03-22 23:01:53 +0800

    日常Fix Bugs

--- push_data.py
+++ push_data.py
@@ -15,7 +15,7 @@ class PushData:
         self.title = '%s市 天气预报' % location_dict['name']
         self.payload['text'] = self.title
         # 推送消息Description
-        self.description = '<# 今日天气\n* 日间(%s):%s℃\n* 夜间(%s):%s℃\n\n# 明日天气\n* 日间(%s):%s℃\n* 夜间(%s):%s℃' % (
+        self.description = '# 今日天气\n* 日间(%s):%s℃\n* 夜间(%s):%s℃\n\n# 明日天气\n* 日间(%s):%s℃\n* 夜间(%s):%s℃' % (
             daily_dict[0]['text_day'], daily_dict[0]['high'], daily_dict[0]['text_night'], daily_dict[0]['low'], daily_dict[1]['text_day'], daily_dict[1]['high'], daily_dict[1]['text_night'], daily_dict[1]['low'])
         self.payload['desp'] = self.description
         requests.get(self.url, params=self.payload)
